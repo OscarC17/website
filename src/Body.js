488c5fbf22d6fcd431d87c8da87b466dc5aad9ff
Oscar Coghlan <oscar.coghlan@gmail.com>
2023-05-31 21:20:04 +1000

    now i need to add my work

--- src/Body.js
+++ src/Body.js
@@ -8,12 +8,25 @@ function Body() {
 		<div className="fullpage">
 			<img id="gimage" src={GradientImg} alt=""/>
 		<div className="bodycontent">
+		<div className="firstpage">
 		<div className="title">
-			<h1>Oscar Coghlan</h1>
-			<h2>(Aspiring) Software Developer</h2>
+			<p id="maintitle">Oscar Coghlan</p>
+			<p id="subsubtitle">Software engineering student with a passion for coding</p>
+			<a href="#About" className="buttonStyle">learn more</a>
+			<p className="pb15vh"> </p>
 		</div>
-			<p id="About" className="contentBox">About:<br/>
-				Hi! I'm Oscar. I really love messing around with computers and creating cool things with them. My language of choice is C (C++ when I feel like it), and I have experience in Python, Java, rust, and C#. I am also well versed in web development, with experience in PHP, CSS, and JavaScript - in fact, this very website is my creation (shocking, I know). Scroll down a bit and you can have a look at some of the things I've made.
+		</div>
+		<div className="divwhitespace78" id="About"> &nbsp; </div>
+			<p className="contentBox"><h2>About Me:</h2>
+				Hi! I'm Oscar. I really love messing around with computers and creating cool things with them. My language of choice is C (C++ when I feel like it), and I have experience in Python, Java, rust, and C#. I am also well versed in web development, with experience in PHP, CSS, and JavaScript. <br/> <br/>
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. <br/><br/> I’m currently open to job opportunities so please feel free to email or connect with me via LinkedIn.
+
+		<br/>
+		<br/>
+		<br/>
+		Some stuff
+		<div className="divwhitespace78" id="Projects"> &nbsp; </div>
+		<h2>My Work:</h2>
 			</p>
 		</div>
 		</div>
--- src/Navbar.js
+++ src/Navbar.js
@@ -8,7 +8,7 @@ function Navbar() {
 		<a href="/index.js">Home</a> &nbsp;&nbsp; <a href="blog.js">Blog</a>
 		<span className="RightAlign">
 		<a href="#About">About</a>&nbsp;&nbsp;&nbsp;&nbsp;
-		<a href="#Portfolio">Portfolio</a>&nbsp;&nbsp;&nbsp;&nbsp;
+		<a href="#Projects">Projects</a>&nbsp;&nbsp;&nbsp;&nbsp;
 		<a href="#Experience">Experience</a>&nbsp;&nbsp;&nbsp;&nbsp;
 		<a href="#Contact">Contact</a>&nbsp;&nbsp;&nbsp;&nbsp;
 		|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
